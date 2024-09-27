0805794657b9dc6f2d3559cad412e3bb500e7a49
sksahil <sksahilnew2021@gmail.com>
2024-09-27 22:18:01 +0530

    modified by sk sahil

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -228,6 +228,15 @@ function ShoppingHome() {
         setOpen={setOpenDetailsDialog}
         productDetails={productDetails}
       />
+
+      {/* Footer */}
+      <footer className="bg-gray-800 text-white py-6 mt-auto">
+        <div className="container mx-auto px-4 text-center">
+          <p>&copy; {new Date().getFullYear()} Shopping Platform. All rights reserved.</p>
+          <p>Contact us: support@shoppingplatform.com</p>
+        </div>
+      </footer>
+
     </div>
   );
 }
